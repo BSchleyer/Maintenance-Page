2f1751d3dc043686f44fd198315ccaca651a8598
BSchleyer <68195428+BSchleyer@users.noreply.github.com>
2020-08-03 14:04:22 +0200

    Add files via upload

---
+++ app/config.php
@@ -0,0 +1,27 @@
+<?php
+
+### Site ###
+$siteName = 'Schleyer-EDV'; // SiteName
+$url = 'http://localhost/maintenance/'; // URL with "/" to the end
+
+### Assets ###
+$cdnUrl = $url.'assets/';
+$imgUrl = $url.'assets/images/';
+
+### Text ###
+$title_page = 'Maintenance'; // index Title
+$text_page = 'Coming Soon'; // index Text
+
+$day = 'Days'; // Text-Box: Day
+$hours = 'Hours'; // Text-Box: Hours
+$minutes = 'Minutes'; // Text-Box: Minutes
+$seconds = 'Seconds'; // Text-Box: Seconds
+
+### Countdown ###
+$c_year = '2022'; // Countdown-Set: Year
+$c_month = '07'; // Countdown-Set: Month
+$c_day = '27'; // Countdown-Set: Day
+$c_hour = '0'; // Countdown-Set: Hour
+$c_minute = '0'; // Countdown-Set: Minute
+$c_second = '0'; // Countdown-Set: Second
+$c_timezone = 'Europe/Berlin'; // Timezones: http://momentjs.com/timezone/
---
+++ includes/head.php
@@ -0,0 +1,29 @@
+<?php
+include 'app/config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?= $currPageName ?> | <?= $siteName ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="<?= $seo_description ?>">
+    <meta name="keywords" content="<?= $seo_keywords ?>">
+    <meta name="author" content="Björn Schleyer & Schleyer-EDV">
+    <meta name="copyright" content="Copyright 2020 Schleyer-EDV | All Rights reserved.">
+    <meta name="email" content="mail@bschleyer.de" />
+    <meta name="website" content="https://www.bschleyer.de/" />
+    <meta name="version" content="v1.2" />
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="<?= $imgUrl ?>icons/favicon.ico"/>
+    <link rel="stylesheet" type="text/css" href="<?= $cdnUrl ?>vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= $cdnUrl ?>fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= $cdnUrl ?>fonts/iconic/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= $cdnUrl ?>vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="<?= $cdnUrl ?>vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= $cdnUrl ?>css/util.css">
+    <link rel="stylesheet" type="text/css" href="<?= $cdnUrl ?>css/main.css">
+    <!--===============================================================================================-->
+</head>
+<body>
